ed825fca6d79ee399224e0c523bfa2326aafb55a
shljessie <sl994@cornell.edu>
2024-04-13 11:02:45 -0700

    update date saving

--- src/components/SonicTiles.jsx
+++ src/components/SonicTiles.jsx
@@ -1083,13 +1083,12 @@ const speakNoTileFocusedMessage = () => {
 
   function getFormattedTimestamp() {
     const now = new Date();
-    const year = now.getFullYear(); // You might not need this.
     const month = (now.getMonth() + 1).toString().padStart(2, '0');
     const day = now.getDate().toString().padStart(2, '0');
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
-    return `${month}.${day}.${hours}:${minutes}:${seconds}`;
+    return `${month}.${day}_${hours}:${minutes}:${seconds}`;
   }
   
 
